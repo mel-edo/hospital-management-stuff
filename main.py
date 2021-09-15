43de4d49b7accf6095ad02f2c3ee23d0d9806284
MeledoJames <90543784+MeledoJames@users.noreply.github.com>
2021-09-15 16:31:29 +0530

    Update main.py

--- main.py
+++ main.py
@@ -3,7 +3,15 @@
 Members:-
 1) Saurabh
 2) Raunak
-3) Sujal topper
+3) Sujal
 
-Title:-
+Title:- Patient information system
+(no. of people registered, vaccine takers, no. of shots, which one is it etc.)
+Program should be able to:-
+-search all records of patients
+-track info of patients
+-add, update, remove patient data and medicine data
+-report generation for patients
+-transactions
+-report generation of patients
 '''
