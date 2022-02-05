1ae04e555548e35ae8b6ebdf717b66c3a65e3883
MeledoJames <90543784+MeledoJames@users.noreply.github.com>
2022-02-05 20:07:41 +0530

    Made tables, database and some additions to the main loop

--- main.py
+++ main.py
@@ -22,10 +22,40 @@ https://docs.google.com/document/d/1UYbQPDa1fvAXFaGIU2_KWfl-VtuCvlOuH5WvQmfIf68/
 '''
 
 import mysql.connector as c
-con = c.connect(host="localhost", user="placeholder", password="placeholder")
+db_user = input("Enter database username:- ")
+db_password = input("Enter database password:- ")
+con = c.connect(host="localhost", user=db_user, password=db_password)
 cur = con.cursor()
+
+# Creating the database and the tables
+# Going to make 3 tables, Patients, Doctors, Services in the hospital
+
+cur.execute("create database if not exists hospital")
+cur.execute("use hospital")
+
+cur.execute("create table if not exists Patients(PatientID int PRIMARY KEY, Name varchar(50), Age int, Gender varchar(20), Phone int, Bloodgroup varchar (10))")
+
+cur.execute("create table if not exists Doctors(DoctorID int PRIMARY KEY, Name varchar(50), Specialization varchar(50), RoomNo int)")
+
+cur.execute("create table if not exists Services(ServiceID int PRIMARY KEY, Services varchar(50))")
 
 # Main loop
 
 while True:
-    pass
+
+    print("Hospital Management system:-\n")
+
+    print("1) Register(patient)\n2) Make an Appointment(patient) \n3) Check Appointments(docotor)\n4) List of doctors\n5) List of services\n6) Edit Data\n7) Exit")
+
+    ch = int(input("Enter one of the numbers(1-7) above to proceed:- "))
+    print()
+
+    if ch not in range(1, 8):
+        print("Please enter from one of the numbers in the range!\n")
+        continue
+
+    else:
+        if ch == 7:
+            print("Thank you for using our program :praydge:")
+
+            break
